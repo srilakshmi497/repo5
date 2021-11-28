9ee951e4ce7ee38d91a8ff71a83c7b45ca5e7e8f
srilakshmi497 <awscourse497@gmail.com>
2021-11-28 21:28:55 +0530

    commited

---
+++ src/main/java/4455/sandeep/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package 4455.sandeep;
+
+import javax.ws.rs.ApplicationPath;
+import javax.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
